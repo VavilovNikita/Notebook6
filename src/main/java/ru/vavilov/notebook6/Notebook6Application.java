dd27b2e645ff6f2348a2c2b09a9bf0842689144f
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2023-07-28 13:26:42 +0500

    initial commit

---
+++ src/main/java/ru/vavilov/notebook6/Notebook6Application.java
@@ -0,0 +1,13 @@
+package ru.vavilov.notebook6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Notebook6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Notebook6Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vavilov/Notebook6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vavilov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Notebook6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
