d9b91e993dba6baa29f6c762ff9ecd186a074b86
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2023-08-12 17:58:49 +0500

    fix review

--- src/main/java/ru/vavilov/notebook6/service/UserDetailService.java
+++ src/main/java/ru/vavilov/notebook6/service/UserDetailService.java
@@ -19,10 +19,8 @@ public class UserDetailService implements UserDetailsService {
 
     @Override
     public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> user = userRepository.findByUsername(username);
-        if (user.isEmpty()) {
-            throw new UsernameNotFoundException("Не удается найти пользователя :" + username);
-        }
-        return new UserDetail(user.get());
+        return userRepository.findByUsername(username)
+                .map(UserDetail::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Не удается найти пользователя :" + username));
     }
 }
