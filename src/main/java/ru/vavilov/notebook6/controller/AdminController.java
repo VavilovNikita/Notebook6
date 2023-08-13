eb229af45361bb0a9033b0b0a11b97107d448485
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2023-08-13 21:26:41 +0500

    try add GlobalAuthentication

--- src/main/java/ru/vavilov/notebook6/controller/AdminController.java
+++ src/main/java/ru/vavilov/notebook6/controller/AdminController.java
@@ -7,34 +7,35 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.vavilov.notebook6.entity.User;
-import ru.vavilov.notebook6.service.UserService;
+import ru.vavilov.notebook6.service.AdminService;
+
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-    private final UserService userService;
+    private final AdminService adminService;
 
     @Autowired
-    public AdminController(UserService userService) {
-        this.userService = userService;
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
     }
 
     @GetMapping()
     public String allUserPage(Model model) {
-        model.addAttribute("user", userService.findAll());
+        model.addAttribute("user", adminService.findAll());
         return "admin/allUserPage";
     }
     @GetMapping("/{id}")
     public String userInfo(@PathVariable("id") int id, Model model) {
-        User user = userService.findById(id);
+        User user = adminService.findById(id);
         model.addAttribute("user", user);
         model.addAttribute("notes", user.getNotes());
         return "admin/userInfoPage";
     }
     @GetMapping("/{id}/edit")
     public String updateUser(Model model, @PathVariable("id") int id) {
-        model.addAttribute("user", userService.findById(id));
+        model.addAttribute("user", adminService.findById(id));
         return "admin/updateUserPage";
     }
     @PatchMapping("/{id}")
@@ -42,12 +43,12 @@ public class AdminController {
         if (bindingResult.hasErrors()) {
             return "admin/updateUserPage";
         }
-        userService.saveUser(user);
+        adminService.saveUser(user);
         return "redirect:/admin";
     }
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable("id") int id) {
-        userService.deleteUser(id);
+        adminService.deleteUser(id);
         return "redirect:/admin";
     }
 }
--- src/main/java/ru/vavilov/notebook6/service/AdminService.java
+++ src/main/java/ru/vavilov/notebook6/service/AdminService.java
@@ -1,37 +1,39 @@
 package ru.vavilov.notebook6.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import ru.vavilov.notebook6.entity.User;
 import ru.vavilov.notebook6.repository.UserRepository;
 
-import java.util.Optional;
 
 @Service
-public class UserService {
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+public class AdminService {
 
     private final UserRepository userRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public AdminService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public User findById(int id) {
         return userRepository.findById(id).orElse(null);
     }
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void saveUser(User user) {
-
         userRepository.save(user);
     }
-
-    public Optional<User> findByEmail(String email) {
-        return userRepository.findByEmail(email);
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteUser(int id) {
+        userRepository.deleteById(id);
     }
 
-    public Optional<User> findByUserName(String username) {
-        return userRepository.findByUsername(username);
-    }
 
 }
