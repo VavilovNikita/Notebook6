7d88032cc132cc1a3a943f44715c40e03135efb6
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2023-09-08 21:38:47 +0500

    add pageable

--- src/main/java/ru/vavilov/notebook6/repository/NotebookRepository.java
+++ src/main/java/ru/vavilov/notebook6/repository/NotebookRepository.java
@@ -1,12 +1,10 @@
 package ru.vavilov.notebook6.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.vavilov.notebook6.entity.Notebook;
 
-import java.util.Optional;
-
 @Repository
-public interface NotebookRepository extends CrudRepository<Notebook, Integer> {
+public interface NotebookRepository extends JpaRepository<Notebook, Integer> {
     Iterable<Notebook> findAllByOrderByPositionDesc();
 }
