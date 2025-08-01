b1e4f18e4a32773cc8df583e0fbcad701303ce45
VavilovNikita <vavilovnik00@mail.ru>
2025-08-01 21:33:32 +0300

    fix

--- src/main/java/ru/vavilov/notebook6/config/WebSecurityConfig.java
+++ src/main/java/ru/vavilov/notebook6/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package ru.vavilov.notebook6.notebook.config;
+package ru.vavilov.notebook6.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
